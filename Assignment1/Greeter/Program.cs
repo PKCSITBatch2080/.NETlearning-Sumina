db448ed72fb462de5663222b07d7a36c7aa84b05
Sumina7s <asumina54@gmail.com>
2024-02-25 20:07:54 +0545

    uppercase

--- Assignment1/Greeter/Program.cs
+++ Assignment1/Greeter/Program.cs
@@ -1,4 +1,18 @@
 ﻿using System;
 
-string fullName = "Sumina Shrestha";
-Console.WriteLine(fullName);
+class Program
+{
+    static void Main()
+    {
+        // Assuming you have a variable named fullName
+        string fullName = "Sumina Shrestha";
+
+        // Create another variable cFullName and initialize it with fullName in uppercase
+        string cFullName = fullName.ToUpper();
+
+        // Print the uppercase name
+        Console.WriteLine("Uppercase Name: " + cFullName);
+
+        
+    }
+}
